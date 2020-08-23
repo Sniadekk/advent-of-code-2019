1dd5ecf5eba75d76cbdead61d524f6632326f88e
Sniadekk <karolgruszka9@gmail.com>
2020-08-23 14:42:14 +0200

    feat: day-6

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,67 +1,30 @@
-use std::collections::HashMap;
+mod graph;
+mod utils;
+use graph::{Graph, Objects};
 use std::fs::read_to_string;
 
-type Objects = HashMap<String, Vec<String>>;
-#[derive(Debug)]
-// Object contains its name and vector of other objects it orbits
-struct Orbit(String, Vec<Orbit>);
-
-fn traverse_orbits(objects: &mut Objects, object_name: &String) -> Orbit {
-    let object_orbits: Vec<Orbit> = objects
-        .get(object_name)
-        .cloned()
-        .map(|orbiters| {
-            orbiters
-                .iter()
-                .map(|orbiter| traverse_orbits(objects, orbiter))
-                .collect()
-        })
-        .unwrap_or(Vec::new());
-
-    Orbit(object_name.clone(), object_orbits)
-}
-
-fn calculate_orbits_number(orbit: &Orbit, com_offset: usize) -> usize {
-    orbit
-        .1
-        .iter()
-        .map(|orbiter| calculate_orbits_number(orbiter, com_offset + 1))
-        .sum::<usize>()
-        + com_offset
-}
-
 fn main() -> std::io::Result<()> {
-    let mut objects: Objects = HashMap::new();
-    read_to_string("input.txt")?.lines().for_each(|line| {
-        let orbited = line.get(0..3).expect("Invalid input");
-        let orbiting = line.get(4..7).expect("Invalid input");
+    let input = read_to_string("input.txt").expect("Input not found");
 
-        match objects.get_mut(orbited) {
-            Some(all) => {
-                all.push(orbiting.to_owned());
-            }
-            None => {
-                objects.insert(orbited.to_owned(), vec![orbiting.to_owned()]);
-            }
-        }
+    let vertices: Vec<(&str, &str)> = input
+        .lines()
+        .map(|line| {
+            let vertices = line.split(")").collect::<Vec<&str>>();
+            (
+                *vertices.get(0).expect("Invalid input"),
+                *vertices.get(1).expect("Invalid input"),
+            )
+        })
+        .collect();
 
-        match objects.get_mut(orbiting) {
-            Some(all) => {
-                all.push(orbited.to_owned());
-            }
-            None => {
-                objects.insert(orbiting.to_owned(), vec![orbited.to_owned()]);
-            }
-        }
-    });
-    // create tree of orbits
-    let orbits = traverse_orbits(&mut objects, &String::from("COM"));
+    let objects = Objects::from(vertices.clone());
+    println!("PART ONE: {}", objects.checksum());
     println!(
-        "ANSWER TO FIRST PART: {:#?}",
-        calculate_orbits_number(&orbits, 0)
+        "PART TWO: {}",
+        objects
+            .distance("YOU", "SAN")
+            .expect("Something went wrong and we couldn't find the smallest distance to santa :(")
+            - 1
     );
-
-    let you_orbits = traverse_orbits(&mut objects, &String::from("YOU"));
-    println!("{:#?}", you_orbits);
     Ok(())
 }
